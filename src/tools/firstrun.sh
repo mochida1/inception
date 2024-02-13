d5f97abb5aa99d8bb712f71885a8088eb8235028
Henrique Mochida <h_mochida@hotmail.com>
2024-02-12 21:49:33 -0300

    feat(make): adicionado nameserver 8.8.4.4 para firstrun

--- src/tools/firstrun.sh
+++ src/tools/firstrun.sh
@@ -2,6 +2,7 @@
 
 # Define the nameserver line to add
 nameserver_line="nameserver 8.8.8.8"
+nameserver_line2="nameserver 8.8.4.4" 
 
 # Define the file path
 file="/etc/resolv.conf"
@@ -14,6 +15,19 @@ if [ -f "$file" ] && [ -r "$file" ]; then
     if ! grep -q "^$nameserver_line" "$file"; then
         # Add the nameserver line at the beginning of the file
         echo "$nameserver_line" | cat - "$file" > temp && mv temp "$file"
+        echo "Added nameserver line to $file"
+    else
+        echo "Nameserver line already exists in $file"
+    fi
+else
+    echo "File $file does not exist or is not readable"
+fi
+
+if [ -f "$file" ] && [ -r "$file" ]; then
+    # Check if the nameserver line already exists in the file
+    if ! grep -q "^$nameserver_line2" "$file"; then
+        # Add the nameserver line at the beginning of the file
+        echo "$nameserver_line2" | cat - "$file" > temp && mv temp "$file"
         echo "Added nameserver line to $file"
     else
         echo "Nameserver line already exists in $file"
